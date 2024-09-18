709338311e4897747a52d5c331e584f6e2baec81
Rohan <rohan151025@gmail.com>
2024-09-18 17:22:05 +0530

    Create a very basic calculator
    created a basic calculator, which can take 2 operands and a single
    operator at a time, nothing fancy. It can add, subtract, multiply,
    divide for now.

--- script.js
+++ script.js
@@ -14,12 +14,23 @@ function divide(first, second) {
     return (first / second);
 }
 
-let firNum;
-let secNum;
+let num1 = '';
+let num2 = '';
 let operator;
+const operators = ['+', '-', '*', '/'];
+// let operInuse = 0;
+// let decInuse = 0;
 
-function operate() {
-    let result;
+function operate(firNum, operator, secNum) {
+    let result = null;
+    firNum = parseFloat(firNum);
+    secNum = parseFloat(secNum);
+
+    if (isNaN(firNum) || isNaN(secNum)) {
+        console.log(`firNum: ${firNum}, secNum: ${secNum}, operator: ${operator}`);
+        return "numbers invalid";
+    }
+
     switch (operator) {
         case '+':
             result = add(firNum, secNum);
@@ -31,8 +42,71 @@ function operate() {
             result = multiply(firNum, secNum);
             break;
         case '/':
-            result = divide(firNum, secNum);
+            if (secNum === 0) {
+                result = "don't do it!";
+            } else {
+                result = divide(firNum, secNum);
+            }
+            break;
+        default:
+            result = "operator invalid";
             break;
     }
     return result;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    buttListeners();
+})
+
+function buttListeners() {
+    let screen = document.querySelector('.calc_screen');
+
+    let clear = document.querySelector('.clear');
+    clear.addEventListener('click', () => {
+        handleClear();
+        screen.textContent = '';
+    })
+
+    let numButts = document.querySelectorAll('.num');
+    numButts.forEach(numButt => {
+        numButt.addEventListener('click', handleNumClick, true);
+    })
+
+    let operat = document.querySelectorAll('.operator');
+    operat.forEach(oper => {
+        oper.addEventListener('click', (e) => {
+            handleOperClick(e);
+        })
+    })
+
+    let equal = document.querySelector('.equals');
+    equal.addEventListener('click', () => {
+        let result = operate(num1, operator, num2);
+        screen.textContent = result;
+        handleClear();
+    })
+
+}
+
+// number is clicked
+function handleNumClick(e) {
+    let butt = e.target.textContent;
+    if (operator) {
+        num2 += butt;
+    }
+    else {
+        num1 += butt;
+    }
+}
+
+function handleClear() {
+    num1 = '';
+    num2 = '';
+    operator = '';
+}
+
+function handleOperClick(e) {
+    operator = e.target.textContent;
+}
+
